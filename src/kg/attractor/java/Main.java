81b33395b2feceb75effe3ae3cac7703ef7d01e2
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-05-02 12:32:51 +0600

    added unnecessary files to .gitignore

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,7 +1,6 @@
 package kg.attractor.java;
 
 import kg.attractor.java.homework.RestaurantOrders;
-import kg.attractor.java.lesson.MovieCollection;
 
 public class Main {
 
@@ -13,11 +12,11 @@ public class Main {
         // это для домашки
         // выберите любое количество заказов, какое вам нравится.
 
-       // var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        // var orders = RestaurantOrders.read("orders_100.json").getOrders();
         //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
         //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
-       // var totalOrder = RestaurantOrders.read("orders_100.json").printOrderSum();
-        RestaurantOrders ro=new RestaurantOrders("orders_100.json");
+        // var totalOrder = RestaurantOrders.read("orders_100.json").printOrderSum();
+        RestaurantOrders ro = new RestaurantOrders("orders_100.json");
         ro.printOrderSum();
         System.out.println();
         ro.printList();
@@ -47,11 +46,6 @@ public class Main {
         ro.productQuantity();
         System.out.println();
         ro.listEmail();
-
-
-
-
-
         // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
     }
 }
